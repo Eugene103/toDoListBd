4c960119d3ce26d22beedc48651fdac83ea72797
Eugene103 <chertyka74@gmail.com>
2024-05-25 18:36:17 +0300

    v1

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 import app from "./app.js";
 
-const {DB_HOST, PORT = 3001} = process.env
+const {DB_HOST, PORT = 3000} = process.env
 mongoose.connect(DB_HOST)
-    .then(() => {
-        app.listen(PORT, () => {
-        console.log(`Server connnect ${PORT}`)
-        })
-    })
-    .catch((err) => {
-        console.log(err)
-        process.exit(1)
-    })  
+  .then(() => {
+  app.listen(PORT, () => {
+  console.log(`Database connection successful`)
+})
+ 
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1)
+})
